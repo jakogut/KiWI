8068bb7ecfdf7546ff72bb6e1494c91e054f5236
Joseph Kogut <joseph.kogut@gmail.com>
2016-02-10 15:48:01 -0800

    Move Menu object to separate module

--- kiwi/install.py
+++ kiwi/install.py
@@ -16,33 +16,6 @@ def detect_blockdevs():
 
     return devices
 
-class Menu(object):
-    def __init__(self, dialog, items, title, caller = None):
-        self.d = dialog
-        self.caller = caller
-
-        self.entries = []
-        self.dispatch_table = {}
-        tag = 1
-
-        self.title = title
-
-        for entry, func in items:
-            self.entries.append(tuple([str(tag), entry]))
-            self.dispatch_table[str(tag)] = func
-            tag += 1
-
-    def run(self, ret=None):
-        code, tag = self.d.menu(self.title, choices=self.entries)
-        if code == self.d.OK: self.dispatch(tag)
-        if ret: ret()
-
-    def dispatch(self, tag):
-        if tag in self.dispatch_table:
-            func = self.dispatch_table[tag]
-            if isinstance(func, Menu):
-                func.run(ret=self.run)
-            else: func()
 
 class WindowsInstallApp(object):
     def __init__(self):
