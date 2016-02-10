f20a8f8d05e641a123062e5ad1f9e2f9959f92b6
Joseph Kogut <joseph.kogut@gmail.com>
2016-02-10 12:40:14 -0800

    Create setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'KiWI',
+    description = 'Killer Windows Installer',
+    author = 'Joseph Kogut',
+    author_email = 'joseph.kogut@gmail.com',
+    url = 'josephkogut.com/yaknet/kiwi.git',
+    packages = ['kiwi'],
+)
