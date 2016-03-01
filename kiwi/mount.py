b91eb0b8b5bd66ea0bf090e6c6e71232c81d6e7a
Joseph Kogut <joseph.kogut@gmail.com>
2016-03-01 15:42:35 -0800

    Add mount.py

---
+++ kiwi/mount.py
@@ -0,0 +1,22 @@
+def mountpoint(path):
+    try:
+        subprocess.check_call(['mountpoint', path],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        return False
+
+    return True
+
+def unmount(path):
+    subprocess.check_call(['umount', path])
+
+def mount(src, dst, mkdir=False, force=False):
+    if mkdir: subprocess.check_call(['mkdir', '-p', dst])
+
+    if mountpoint(dst):
+        logger.warning('Destination %s is already a mountpoint' % dst)
+        if force: unmount(dst)
+        else: return
+
+    subprocess.check_call(['mount', src, dst])
+
