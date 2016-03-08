1dae3716e9a7e1eb28927e705f84ba73f29f52c1
Joseph Kogut <joseph.kogut@gmail.com>
2016-03-08 14:43:48 -0800

    Access cluster_size as attribute

--- kiwi/install.py
+++ kiwi/install.py
@@ -167,8 +167,7 @@ class WindowsInstallApp(object):
             self.system_part = self.install_drive + '1'
 
     def auto_format(self):
-        cluster_size = 4096
-        subprocess.check_call(['mkfs.ntfs', '-Q', '-c', str(cluster_size), self.system_part])
+        subprocess.check_call(['mkfs.ntfs', '-Q', '-c', str(self.cluster_size), self.system_part])
         if self.uefi: subprocess.check_call(['mkfs.msdos', '-F32', self.boot_part])
 
         self.logger.info('Sucessfully partitioned installation drive')
